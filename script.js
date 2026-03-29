d9ae05a6ded7cb7626c55c4056c6532385b4b4b8
Hector Barrientos <m.hec12@gmail.com>
2026-03-28 19:03:39 -0500

    Add getComputerChoice and rockPaperScissors functions
    getComputerChoice returns a random choice
    rockPaperScissors maps a number to a string value

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, World!');
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3) + 1;
+  return rockPaperScissors(random);
+}
+
+function rockPaperScissors(choice) {
+  switch (choice) {
+    case 1:
+      return 'Rock';
+    case 2:
+      return 'Paper';
+    case 3:
+      return 'Scissors';
+  }
+}
