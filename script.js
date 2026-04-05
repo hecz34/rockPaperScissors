439ead94f93275b0f6b8a0eb85fac711b0d7fc29
Hector Barrientos <m.hec12@gmail.com>
2026-04-05 16:34:57 -0500

    Add playRound function
    Play single round, determine winner, and update score

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function getComputerChoice() {
   let random = Math.floor(Math.random() * 3) + 1;
   switch (random) {
     case 1:
-      return 'Rock';
+      return 'rock';
     case 2:
-      return 'Paper';
+      return 'paper';
     case 3:
-      return 'Scissors';
+      return 'scissors';
   }
 }
 
@@ -15,6 +15,26 @@ function getHumanChoice() {
     'Enter your choice:'
   );
   return userInput;
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log('Tie Nobody Wins');
+  }
+  else if (
+    humanChoice === 'rock' && computerChoice === 'scissors' ||
+    humanChoice === 'scissors' && computerChoice === 'paper' ||
+    humanChoice === 'paper' && computerChoice === 'rock'
+  ) {
+    console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    humanScore += 1;
+  }
+  else {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore += 1;
+  }
 }
 
 let humanScore = 0;
